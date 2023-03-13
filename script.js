43837796f3134bd8c301eb670ed141e7d8cc1201
Andrew <48330373+techni-ca@users.noreply.github.com>
2023-03-13 11:36:53 -0600

    not sure!

--- script.js
+++ script.js
@@ -4,64 +4,68 @@ const addbook = document.querySelector('#addbook')
 function Book (title, author) {
   this.title = title
   this.author = author
+  this.read = false
 }
 
 function addBookToLibrary (event) {
   const title = document.querySelector('#title')
   const author = document.querySelector('#author')
 
-  if (
-    title.value !== '' &&
-    author.value !== '' &&
-    findBook(title.value, author.value) === false
-  ) {
+  if (title.value !== '' && author.value !== '') {
     const newBook = new Book(title.value, author.value)
-    myLibrary.push(newBook)
-    displayBook(newBook)
+    let index = myLibrary.length
+    for (let i = 0; i < index; i++) {
+      if (myLibrary[i] === null) {
+        index = i
+        break
+      }
+    }
+    myLibrary[index] = newBook
+    displayBook(index)
     title.value = ''
     author.value = ''
   }
   event.preventDefault()
 }
 function removeBookFromLibrary (event) {
-  const title = event.srcElement.nextElementSibling.textContent
-  const author =
-    event.srcElement.nextElementSibling.nextElementSibling.textContent
   const parentClass = event.srcElement.parentElement
+  const bookIndex = parentClass.dataset.index
   parentClass.remove()
 
-  const bookIndex = findBook(title, author)
-  if (bookIndex !== false) {
-    myLibrary.splice(bookIndex, 1)
-  }
+  myLibrary[bookIndex] = null
 }
-function findBook (title, author) {
-  for (let i = 0; i < myLibrary.length; i++) {
-    if (myLibrary[i].title === title && myLibrary[i].author === author) {
-      return i
-    }
-  }
-  return false
+function toggleReadStatus (event) {
+  const parentClass = event.srcElement.parentElement
+  const bookIndex = parentClass.dataset.index
+  myLibrary[bookIndex].read = !myLibrary[bookIndex].read
+  event.srcElement.textContent = myLibrary[bookIndex].read ? 'Read' : 'Unread'
 }
 
-function displayBook (book) {
+function displayBook (index) {
+  const book = myLibrary[index]
   const libraryClass = document.querySelector('.library')
   const deleteClass = document.createElement('button')
   deleteClass.className = 'delete'
   deleteClass.textContent = 'X'
   const bookClass = document.createElement('div')
   bookClass.className = 'book'
+  bookClass.dataset.index = index.toString()
   const titleClass = document.createElement('div')
   titleClass.className = 'title'
   titleClass.textContent = book.title
   const authorClass = document.createElement('div')
   authorClass.className = 'author'
   authorClass.textContent = book.author
+  const readClass = document.createElement('button')
+  readClass.className = 'read'
+  readClass.textContent = 'Unread'
   libraryClass.appendChild(bookClass)
   bookClass.appendChild(deleteClass)
   bookClass.appendChild(titleClass)
   bookClass.appendChild(authorClass)
+  bookClass.appendChild(readClass)
   deleteClass.addEventListener('click', removeBookFromLibrary)
+  readClass.addEventListener('click', toggleReadStatus)
 }
 
 addbook.addEventListener('click', addBookToLibrary)
@@ -69,4 +73,6 @@ addbook.addEventListener('click', addBookToLibrary)
 myLibrary.push(new Book('The Hobbit', 'J.R.R. Tolkien'))
 myLibrary.push(new Book('Fall of Giants', 'Ken Follett'))
 myLibrary.push(new Book('The SAS Survival Handbook', 'John Wiseman'))
-myLibrary.forEach(displayBook)
+for (let i = 0; i < myLibrary.length; i++) {
+  displayBook(i)
+}
